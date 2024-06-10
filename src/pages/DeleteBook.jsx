4ac3b32f71f963b285471e92517b9cf2b02333d1
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-10 15:13:35 +0530

    Update DeleteBook.jsx

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete('https://book-store-backend-kappa-coral.vercel.app/books/${id}')
+      .delete(`https://book-store-backend-kappa-coral.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
