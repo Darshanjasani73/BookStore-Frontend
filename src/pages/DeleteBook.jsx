b77ebe7a5f195a0a26e4a647bd46b9aa00fbac5e
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-11 12:44:14 +0530

    Update DeleteBook.jsx

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -15,7 +15,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`https://bookstore-backend-qjzq.onrender.com/books/${id}`)
+      .delete(`${backendUrl}/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
@@ -23,29 +23,27 @@ const DeleteBook = () => {
       })
       .catch((error) => {
         setLoading(false);
-        // alert('An error happened. Please Chack console');
         enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
-  
-  return (
-    <div className='p-4'>
-      <BackButton />
-      <h1 className='text-3xl my-4'>Delete Book</h1>
-      {loading ? <Spinner /> : ''}
-      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-        <h3 className='text-2xl'>Are You Sure You want to delete this book?</h3>
 
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : null}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-full max-w-lg p-8 mx-auto">
+        <h3 className="text-2xl text-center">Are You Sure You want to delete this book?</h3>
         <button
-          className='p-4 bg-red-600 text-white m-8 w-full'
+          className="p-4 bg-red-600 text-white mt-8 w-full"
           onClick={handleDeleteBook}
         >
           Yes, Delete it
         </button>
       </div>
-    </div> 
-  )
-}
+    </div>
+  );
+};
 
 export default DeleteBook;
