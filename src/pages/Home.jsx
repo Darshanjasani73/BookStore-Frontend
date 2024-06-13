8a4a902f73cf9ccd5dc048aa61664edab0b05043
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-13 12:20:54 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,9 +27,7 @@ const Home = () => {
     <div className='p-4'>
 
       <div className='flex justify-between items-center'>
-        <div className='flex justify-center'>
-  <h1 className='text-3xl my-8 text-center'>Books Store</h1>
-</div>
+  <h1 className='text-3xl my-8 text-bold'>Books Store</h1>
         <Link to='/books/create'>
           <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
